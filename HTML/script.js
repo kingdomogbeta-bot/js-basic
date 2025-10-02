c6c5c8f6c1c2e38a719633d95b07f70c226d40c1
kingdom <kingdomogbeta@gmail.com>
2025-10-02 10:45:41 +0100

    commit

---
+++ HTML/script.js
@@ -0,0 +1,42 @@
+function ability() {
+const studentName = document.getElementById("studentName").value;
+const assignment1 = Number(document.getElementById("assignment1").value);
+const assignment2 = Number(document.getElementById("assignment2").value);
+const finalExam = Number(document.getElementById("finalexam").value);
+
+
+if (assignment1 < 0){
+     console.log ("please input a number.")
+}
+if (assignment2 < 0) {
+    console.log ("please input a number.")
+}
+if (finalExam < 0) {
+    console.log ("please input a number.")
+}
+
+
+const assignmentAverage = (assignment1 + assignment2) / 2;
+
+
+const finalGrade = (assignmentAverage * 0.4) + (finalExam * 0.6);
+
+const hasPassed = finalGrade >= 70;
+
+
+const isExcellent = hasPassed && finalExam >= 90;
+
+
+console.log("Student Name:", studentName);
+console.log("Final Grade:", finalGrade);
+console.log("Passed:", hasPassed);
+console.log("Excellent Performance:", isExcellent);
+
+if (hasPassed) {
+    alert("Congratulations " + studentName + "! You have passed");
+}else {
+    alert("Sorry " + studentName + "you are a big fool .. you have failed. you will repeat");
+}
+
+document.getElementById("result").innerText = "Student Name: " + studentName + "\nFinal Grade: " + finalGrade + "\nPassed: " + hasPassed + "\nExcellent Performance: " + isExcellent;
+}
